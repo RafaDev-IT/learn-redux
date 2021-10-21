2330f55822d7ef8af68646c232372745a4a66b0a
Rafael Martinez <rafaelvady.q@gmail.com>
2021-10-20 21:19:23 -0600

    🎉 aprendiendo redux con la pokeApi

---
+++ src/redux/pokeDuck.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+//constantes
+const datainitial ={
+    array:[]
+}
+
+//types
+const GET_POKEMONS_SUCCESS ='GET_POKEMONS_SUCCESS'
+
+//reducer
+export default function  pokeReducer(state=datainitial, action){
+    switch(action.type){
+        case GET_POKEMONS_SUCCESS:
+            return {...state, array: action.payload }
+        default:
+            return state
+    }
+}
+
+//acciones
+export const getPokemonsAction = ()=> async(dispatch, getState) => {
+    try {
+        const res = axios.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0');
+        dispatch({
+            type:GET_POKEMONS_SUCCESS,
+            payload: res.data.results
+        })
+        //res.data.results
+    } catch (error) {
+        console.log('error :>> ', error);        
+    }
+}
